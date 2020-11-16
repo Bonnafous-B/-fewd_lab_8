c1bafb371ed1942c53d10c7aa2f370d9ee2dc044
Bonnafous-B <74108740+Bonnafous-B@users.noreply.github.com>
2020-11-16 18:03:42 +0000

    fewd_lab8_EBB

---
+++ Script_.js
@@ -0,0 +1,21 @@
+//ex3
+let planets = ('Xendar','Dune', 'Krypton', 'Namek','Terra' );
+
+//ex4
+console.log(planets [0]);
+console.log(planets [1]);
+console.log(planets [2]);
+console.log(planets [3]);
+console.log(planets [4]);
+
+//ex7
+var hello = function (a){
+    a = 'Hello World !';
+    return a ;
+    
+    
+}
+
+
+//ex8
+console.log(hello());
